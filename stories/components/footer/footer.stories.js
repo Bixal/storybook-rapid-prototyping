7e13680741d6241963bc56730bb7654f82a26ae0
James Mejia <james.mejia@gsa.gov>
2024-08-04 17:57:08 -0500

    Add USWDS slim footer

---
+++ stories/components/footer/footer.stories.js
@@ -0,0 +1,11 @@
+import { fn } from "@storybook/test";
+import Footer from "./footer.html.twig";
+import closeIcon from "@uswds/uswds/img/usa-icons/close.svg";
+
+export default {
+  title: "Components/Footer",
+  args: {},
+  component: Footer,
+};
+
+export const Default = {};
