6e82a2402147f7a4b164252e9a661d4ce46b4789
James Mejia <james.mejia@gsa.gov>
2024-08-04 17:50:44 -0500

    Add USWDS styles & extended header component

---
+++ stories/components/header/header.stories.js
@@ -0,0 +1,11 @@
+import { fn } from "@storybook/test";
+import Header from "./header.html.twig";
+import closeIcon from "@uswds/uswds/img/usa-icons/close.svg";
+
+export default {
+  title: "Components/Header",
+  args: {},
+  component: Header,
+};
+
+export const Default = {};
