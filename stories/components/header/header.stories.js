26613f630fb94fad1920a03416692fe6c6bf6b6c
James Mejia <james.mejia@gsa.gov>
2024-08-04 17:56:23 -0500

    Remove boilerplate from header

--- stories/components/header/header.stories.js
+++ stories/components/header/header.stories.js
@@ -1,6 +1,4 @@
-import { fn } from "@storybook/test";
 import Header from "./header.html.twig";
-import closeIcon from "@uswds/uswds/img/usa-icons/close.svg";
 
 export default {
   title: "Components/Header",
